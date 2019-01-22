4b00d1824b4656684619fc74b5d1455d8eb6f84d
Martin Parat <marcin.parat@openjawtech.com>
2019-01-22 15:59:03 +0100

    A new generic method.

--- src/generics/Methods.java
+++ src/generics/Methods.java
@@ -1,5 +1,8 @@
 package generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Methods {
 
     public static void main(String[] args) {
@@ -10,10 +13,26 @@ public class Methods {
         Float aFloat = printAndReturn(liczba);
         Double aDouble = printAndReturn(liczba2);
         String aString = printAndReturn(text);
+
+        List<String> lista = new ArrayList<>();
+        lista.add("0");
+        lista.add("1");
+        lista.add("1");
+        lista.add("2");
+
+        modifyList(lista);
     }
 
     public static <T> T printAndReturn(T object) {
         System.out.println(object);
         return object;
+    }
+
+    public static <T> void modifyList(List<T> lista) {
+        T element = lista.get(0); // można pobrać
+        lista.set(1, element); // można ustawić
+        lista.remove(lista.size() - 1); // można usunąć\
+        lista.add(element); // można dodać
+        System.out.println(lista);
     }
 }
