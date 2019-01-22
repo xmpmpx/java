f15daea60d0637732c4cb2447f5af1235b0c5f9d
Martin <xmpmpx@gmail.com>
2019-01-22 20:21:04 +0100

    Generics - Type Parameter

---
+++ src/generics/TypeParameter.java
@@ -0,0 +1,14 @@
+package generics;
+
+public class TypeParameter {
+
+    // METODY
+    // Parametr typu dla metod może występować wszędzie.
+    // Zarówno dla metody statycznej jak i niestatycznej
+    // Paramter typu dla metody obejmuje wszystko od jej początku do końca
+
+    // KLASY
+    // Tylko w kontekście niestatycznym, nawet w inicjalizatorze niestatycznym
+    // NIE MOŻĘ kontekście statycznym (w typie enum też nie, ani w zagnieżdżonym interfejsie klasy,
+    // gdyż są one niejawnie statyczne)
+}
