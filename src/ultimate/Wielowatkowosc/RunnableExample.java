9767e8686ab0111376cc3ee3452cda07228db05b
Martin Parat <marcin.parat@openjawtech.com>
2019-10-03 13:55:40 +0200

    Wątki - Thread, Runnable

---
+++ src/ultimate/Wielowatkowosc/RunnableExample.java
@@ -0,0 +1,34 @@
+package ultimate.Wielowatkowosc;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+        System.out.println("Glówny wątek: " + Thread.currentThread().getName());
+        Thread myRunnable = new Thread(new MyRunnable(), "MyRunnable");
+        myRunnable.start();
+
+        MyThread watek_thread = new MyThread("Watek Thread");
+        watek_thread.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Super Anonimowy Wątek");
+            }
+        }).start();
+
+        Runnable super_anonimowy_wątek_lambda = () -> System.out.println("Super Anonimowy Wątek Lambda");
+        new Thread(super_anonimowy_wątek_lambda).start();
+    }
+}
+
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Wątek: " + Thread.currentThread().getName());
+    }
+}
+
+//Runnable lepsze, niż Thread
+//Możliwość dziedziczenia
+//Deklaracja w jednej linii w JAVA 8
---
+++ src/ultimate/Wielowatkowosc/ThreadExample.java
@@ -0,0 +1,36 @@
+package ultimate.Wielowatkowosc;
+
+import java.util.stream.IntStream;
+
+public class ThreadExample {
+
+    //Proces - to program działający w komputerze
+    //Każdy proces ma przydzieloną swoją pamięć.
+    //Każdy proces posiada co najmniej jeden wątek - wątek główny
+    //Można dodawać kolejne wątki
+    //Złota zasada - najpierw program sekwencyjny, następnie dzielenie na wątki i monitorowanie
+
+    //2 sposoby na utworzenie wątku - rozszerzenie Thread
+
+    public static void main(String[] args) {
+        System.out.println("Główny wątek: " + Thread.currentThread().getName());
+        MyThread myThread = new MyThread("My Thread nr.1");
+        MyThread myThread2 = new MyThread("My Thread nr.2");
+        myThread.start(); //start tworzy nowy wątek, nie wywyoływać metody run, gdyż będzie ten sam jeden wątek
+        myThread2.start();
+    }
+}
+
+class MyThread extends Thread {
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Mój wątek: " + Thread.currentThread().getName());
+
+        IntStream.rangeClosed(1, 10).forEach(i -> System.out.println(i + " | Wątek: " + Thread.currentThread().getName()));
+    }
+}
