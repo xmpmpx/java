a6d1f81b60a849f6c8c400d45dcb4acb05465c41
Martin Parat <marcin.parat@openjawtech.com>
2019-10-08 11:19:11 +0200

    ScheduledExecutor

---
+++ src/ultimate/ScheduledExecutor.java
@@ -0,0 +1,32 @@
+package ultimate;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutor {
+
+    public static void main(String[] args) {
+
+        Runnable runnable1 = () -> {
+            System.out.println("Wątek: " + Thread.currentThread().getName());
+            try {
+                for (int i = 0; i <= 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + " | " + i);
+                    TimeUnit.MILLISECONDS.sleep(300);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+        //executor.schedule(runnable1, 5, TimeUnit.SECONDS);
+        //executor.schedule(runnable1, 7, TimeUnit.SECONDS); - dobre, ale wykonanie z opóżnieniem tylko raz
+
+        executor.scheduleAtFixedRate(runnable1, 1, 6, TimeUnit.SECONDS); //co jakiś interwał czasu, działą cały czas, nie może być shutdown
+        executor.scheduleAtFixedRate(runnable1, 3, 6, TimeUnit.SECONDS); //co jakiś interwał czasu, działą cały czas, nie może być shutdown
+
+        //executor.shutdown();
+    }
+}
