73010a31232a0470f2868936dab236d2aee06b9f
Martin Parat <marcin.parat@openjawtech.com>
2019-10-08 10:19:02 +0200

    FixedThreadPoolExecutor

---
+++ src/ultimate/wielowatkowosc/FixedThreadPoolExecutor.java
@@ -0,0 +1,52 @@
+package ultimate.wielowatkowosc;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class FixedThreadPoolExecutor {
+
+    public static void main(String[] args) {
+        System.out.println("Główny wątek: " + Thread.currentThread().getName());
+
+        //ExecutorService - service pomagający nam zarządzenie dużą ilością wątków
+        ExecutorService executorService = Executors.newFixedThreadPool(2); //określona z góry liczba wątków
+
+        Runnable runnable1 = () -> {
+            try {
+                System.out.println("Wątek: " + Thread.currentThread().getName());
+                System.out.println("Pracuję");
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Runnable runnable2 = () -> {
+            try {
+                System.out.println("Wątek: " + Thread.currentThread().getName());
+                System.out.println("Gotuję");
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Runnable runnable3 = () -> {
+            try {
+                System.out.println("Wątek: " + Thread.currentThread().getName());
+                System.out.println("Modlę się");
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+
+        executorService.submit(runnable1);
+        executorService.submit(runnable2);
+        executorService.submit(runnable3);
+
+        executorService.shutdown(); //czeka na zakończenie
+    }
+}
