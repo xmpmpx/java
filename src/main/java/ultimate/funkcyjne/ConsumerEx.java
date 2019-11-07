2b942f39232c35f91cf1bba373a34e043894e331
Martin Parat <marcin.parat@openjawtech.com>
2019-11-07 11:58:16 +0100

    Functional - Consumer.

---
+++ src/main/java/ultimate/funkcyjne/ConsumerEx.java
@@ -0,0 +1,29 @@
+package ultimate.funkcyjne;
+
+import java.util.function.IntConsumer;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class ConsumerEx {
+
+    public static void main(String[] args) {
+
+        IntPredicate mul2Over5 = a -> a > 5;
+
+        IntConsumer print = integer -> System.out.print(integer + " ");
+
+        IntConsumer multiply = integer -> {
+            if (mul2Over5.test(integer)) {
+                integer *= 2;
+            }
+            System.out.print(integer + " ");
+        };
+
+        IntStream.rangeClosed(1, 10).forEach(print::accept);
+        System.out.println();
+        IntStream.rangeClosed(1, 10).forEach(multiply::accept);
+        System.out.println();
+        IntStream.rangeClosed(1, 10).forEach(print.andThen(multiply));
+
+    }
+}
