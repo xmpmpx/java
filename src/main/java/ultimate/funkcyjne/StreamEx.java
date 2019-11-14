1ec2a5615628eb965e88b0a2468e277b11615a9c
Martin Parat <marcin.parat@openjawtech.com>
2019-11-14 13:07:18 +0100

    Stream

--- src/main/java/ultimate/funkcyjne/StreamEx.java
+++ src/main/java/ultimate/funkcyjne/StreamEx.java
@@ -1,8 +1,11 @@
 package ultimate.funkcyjne;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamEx {
@@ -13,7 +16,7 @@ public class StreamEx {
 
         Stream<Integer> stream = Arrays.asList(1, 2, 3, 4, 5).stream();
 
-        Consumer<Integer> print = a -> System.out.print(a + " ");
+        Consumer<Number> print = a -> System.out.print(a + " ");
 
         Stream.generate(() -> new Random().nextInt(49) + 1).limit(6).forEach(print);
         System.out.println();
@@ -23,5 +26,31 @@ public class StreamEx {
 
         Stream.iterate(0, i -> i < 10, i -> i + 1).filter(integer -> integer % 2 == 0).forEach(print);
         System.out.println();
+
+        IntStream.rangeClosed(5, 19).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+
+        DoubleStream.generate(() -> new Random().nextDouble()).limit(5).forEach(i -> System.out.print(i + " "));
+        System.out.println();
+
+        //---------------------------------
+        long s = System.currentTimeMillis();
+        HashSet<Integer> nums = new HashSet<>();
+        Stream.iterate(new Random().nextInt(49) + 1, i -> nums.size() < 6, i -> new Random().nextInt(49) + 1)
+                .filter(nums::add)
+                .forEach(integer -> System.out.print(integer + " "));
+        System.err.println(System.currentTimeMillis() - s);
+
+        System.out.println();
+
+        s = System.currentTimeMillis();
+        HashSet<Integer> numbers = new HashSet<>();
+        do {
+            int r = new Random().nextInt(49) + 1;
+            if (numbers.add(r)) {
+                System.out.print(r + " ");
+            }
+        } while (numbers.size() < 6);
+        System.err.println(System.currentTimeMillis() - s);
     }
 }
