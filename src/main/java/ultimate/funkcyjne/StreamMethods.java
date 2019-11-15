2b2f4beca9554bc360806bcbd8a10ab78b8e8f64
Martin Parat <marcin.parat@openjawtech.com>
2019-11-15 08:46:15 +0100

    Reduce

--- src/main/java/ultimate/funkcyjne/StreamMethods.java
+++ src/main/java/ultimate/funkcyjne/StreamMethods.java
@@ -1,5 +1,7 @@
 package ultimate.funkcyjne;
 
+import java.util.stream.IntStream;
+
 public class StreamMethods {
     public static void main(String[] args) {
 
@@ -18,6 +20,14 @@ public class StreamMethods {
         boolean anyTomek = Martini.getData().stream().map(martini -> martini.name).anyMatch(s -> s.equals("Tomek")); // jakikolwiek
         boolean anyOver10 = Martini.getData().stream().map(martini -> martini.age).allMatch(s -> s > 10); // wszystkie
         boolean noneOver60 = Martini.getData().stream().map(martini -> martini.age).noneMatch(s -> s > 60); // żaden
+
+        System.out.println();
+
+        // Reduce - redukuje strumien do jednej wartości
+        int silnia = IntStream.iterate(1, v -> v <= 5, v -> v + 1).reduce(1, (left, right) -> left * right);
+        System.out.println("Suma: " + silnia);
+
+        IntStream.iterate(1, v -> v <= 5, v -> v + 1).reduce((left, right) -> left * right).ifPresent(System.out::println);
 
     }
 }
