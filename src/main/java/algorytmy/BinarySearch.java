3219e00c9adeb92e1a3ecf9132fd748c5c894883
Martin <xmpmpx@gmail.com>
2025-07-23 22:43:28 +0200

    zadanka

--- src/main/java/algorytmy/BinarySearch.java
+++ src/main/java/algorytmy/BinarySearch.java
@@ -11,6 +11,8 @@ public class BinarySearch {
         String[] words = {"Ala", "Bartek", "Celina", "Dominika", "Halina", "Justyna", "Marcin", "Sara", "Wiki", "Zuzia"};
 
         binarySearch(words, 0, words.length - 1, "Marcin");
+        System.out.println("----");
+        binarySearch(words, "Marcin");
 
     }
 
@@ -28,5 +30,24 @@ public class BinarySearch {
         } else if (array[index].compareTo(value) < 0) {
             binarySearch(array, index + 1, right, value);
         }
+    }
+
+    public static void binarySearch(String[] array, String value) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int index = (left + right) / 2;
+            System.out.println("Odwiedzam: " + array[index]);
+            if (value.equals(array[index])) {
+                System.out.println("Znaleziono: " + value + " - index: " + index);
+                return;
+            } else if (array[index].compareTo(value) > 0) {
+                right = index - 1;
+            } else if (array[index].compareTo(value) < 0) {
+                left = index + 1;
+            }
+        }
+        System.out.println("Nie znaleziono");
     }
 }
